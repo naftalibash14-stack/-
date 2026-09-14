588c9eb37eac81837a01972857f8eb75b82562c6
כמ"ז <naftalibash14@gmail.com>
2026-09-14 22:43:22 +0300

    Update cache version to v10.6 in service worker

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v10.4'; // תעלה את הגרסה כאן כל פעם שאתה מעדכן קבצים
+const CACHE_NAME = 'app-cache-v10.6'; // תעלה את הגרסה כאן כל פעם שאתה מעדכן קבצים
 
 const ASSETS_TO_CACHE = [
   './index.html',
