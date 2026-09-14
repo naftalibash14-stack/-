29c307ad4e182a56a847a88454b2f9d5307a8b65
כמ"ז <naftalibash14@gmail.com>
2026-09-14 09:52:44 +0300

    Update cache version to v10.2

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v10.1'; // תעלה את הגרסה כאן כל פעם שאתה מעדכן קבצים
+const CACHE_NAME = 'app-cache-v10.2'; // תעלה את הגרסה כאן כל פעם שאתה מעדכן קבצים
 
 const ASSETS_TO_CACHE = [
   './index.html',
